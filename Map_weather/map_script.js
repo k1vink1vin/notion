7d95f2149edf0a28f77900a57a433ac14ce04e89
k1vink1vin <32840310+k1vink1vin@users.noreply.github.com>
2024-05-09 23:28:12 +0800

    Update and rename script.js to map_script.js

--- Map_weather/map_script.js
+++ Map_weather/map_script.js
@@ -1,30 +1,10 @@
-//weather weight
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-
-//change time
-
-var date = new Date();
-var options = {
-  hour: '2-digit',   //(e.g., 02)
-  minute: '2-digit', //(e.g., 02)          
-  hour12: false,     // 24 小時制
-  timeZone: 'Asia/Taipei' // 美國/紐約
-};
-var wea=document.getElementsByClassName("weatherwidget-io")
-options.timeZone='Asia/Taipei'
-wea[0].setAttribute("data-label_1",date.toLocaleTimeString('en-GB', options));
-options.timeZone='Asia/Tokyo'
-wea[1].setAttribute("data-label_1",date.toLocaleTimeString('en-GB', options));
-options.timeZone='Europe/London'
-wea[2].setAttribute("data-label_1",date.toLocaleTimeString('en-GB', options));
-options.timeZone='America/New_York'
-wea[3].setAttribute("data-label_1",date.toLocaleTimeString('en-GB', options));
-
 //Map drawing
 
 let rwdSvgWidth = parseInt(d3.select('.RWDChart').style('width')),
-    divh= document.getElementsByClassName('left'),
-    rwdSvgHeight= rwdSvgWidth;
+let divh = document.getElementsByClassName("weatherwidget-io");
+let rwdSvgHeight =  divh[0].clientHeight*2;
+
+console.log("resize" + rwdSvgWidth + "x" + rwdSvgHeight);
 
   const svg = d3.select('.RWDChart')
                 .append('svg')
