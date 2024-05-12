a93f92cb212082aa33238c1a1c98eca27015c043
k1vink1vin <32840310+k1vink1vin@users.noreply.github.com>
2024-05-12 21:17:31 +0800

    Update weather_script.js

--- Map_weather/weather_script.js
+++ Map_weather/weather_script.js
@@ -10,25 +10,3 @@
   }
 })(document, "script", "weatherwidget-io-js");
 
-var timeout_date = window.setInterval(( () => Update_date() ), 1000);
-
-//change time
-function Update_date(){
-console.log("UpdateDate");
-var date = new Date();
-var options = {
-  hour: "2-digit", //(e.g., 02)
-  minute: "2-digit", //(e.g., 02)
-  hour12: false, // 24 小時制
-  timeZone: "Asia/Taipei" // 美國/紐約
-};
-var wea = document.getElementsByClassName("weatherwidget-io");
-options.timeZone = "Asia/Taipei";
-wea[0].setAttribute("data-label_1", date.toLocaleTimeString("en-GB", options));
-options.timeZone = "Asia/Tokyo";
-wea[1].setAttribute("data-label_1", date.toLocaleTimeString("en-GB", options));
-options.timeZone = "Europe/London";
-wea[2].setAttribute("data-label_1", date.toLocaleTimeString("en-GB", options));
-options.timeZone = "America/New_York";
-wea[3].setAttribute("data-label_1", date.toLocaleTimeString("en-GB", options));
-}
